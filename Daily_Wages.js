f35fcf77249b02e94dfdda4d1fe6642bdba38e4c
unknown <chaturgaurav35@gmail.com>
2025-03-20 22:49:22 +0530

    added working hours function

--- Daily_Wages.js
+++ Daily_Wages.js
@@ -18,5 +18,17 @@ switch (empCheck) {
         empHrs = 0;
 }
 
+ // UC-3 function
+function getWorkingHours(empCheck) {
+    switch (empCheck) {
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+        default:
+            return 0;
+    }
+}
+
 let empWage = empHrs * WAGE_PER_HOUR;
 console.log("Emp Wage: " + empWage);
